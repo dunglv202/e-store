ce35a8fcf64f19644b8c7ef98bf0875ffb9c17c5
Luu Van Dung <admin@dunglv202.onmicrosoft.com>
2022-09-29 13:51:03 +0700

    secure app/api endpoints

--- src/main/java/com/example/shopdemo/security/SecurityConfig.java
+++ src/main/java/com/example/shopdemo/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.shopdemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,8 +35,45 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf()
                     .disable()
                 .authorizeRequests()
+
+                // cart
                 .mvcMatchers("/api/v1/carts/**")
                     .hasRole("CUSTOMER")
+
+                // order
+                .mvcMatchers("/api/v1/orders/**")
+                    .hasRole("CUSTOMER")
+
+                // review
+                .mvcMatchers("/api/v1/reviews/**")
+                    .hasRole("CUSTOMER")
+
+                // product management
+                .mvcMatchers(HttpMethod.POST, "/api/v1/products/**")
+                    .hasRole("STOCK_MANAGER")
+                .mvcMatchers(HttpMethod.PUT, "/api/v1/products/**")
+                    .hasRole("STOCK_MANAGER")
+                .mvcMatchers(HttpMethod.DELETE, "/api/v1/products/**")
+                    .hasRole("STOCK_MANAGER")
+                .mvcMatchers(HttpMethod.POST, "/api/v1/products/*/images/**")
+                    .hasAnyRole("STOCK_MANAGER", "SALE_EMPLOYEE")
+                .mvcMatchers(HttpMethod.DELETE, "/api/v1/products/*/images/**")
+                    .hasAnyRole("STOCK_MANAGER", "SALE_EMPLOYEE")
+
+                // brands and categories
+                .mvcMatchers(HttpMethod.POST, "/api/v1/brands/**")
+                    .hasRole("STOCK_MANAGER")
+                .mvcMatchers(HttpMethod.PUT, "/api/v1/brands/**")
+                    .hasRole("STOCK_MANAGER")
+                .mvcMatchers(HttpMethod.DELETE, "/api/v1/brands/**")
+                    .hasRole("STOCK_MANAGER")
+                .mvcMatchers(HttpMethod.POST, "/api/v1/categories/**")
+                    .hasRole("STOCK_MANAGER")
+                .mvcMatchers(HttpMethod.PUT, "/api/v1/categories/**")
+                    .hasRole("STOCK_MANAGER")
+                .mvcMatchers(HttpMethod.DELETE, "/api/v1/categories/**")
+                    .hasRole("STOCK_MANAGER")
+
                 .anyRequest()
                     .permitAll()
                 .and()
