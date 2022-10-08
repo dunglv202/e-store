4b8b0f19db4d47d2a781b83914c9465707504a22
Luu Van Dung <admin@dunglv202.onmicrosoft.com>
2022-10-08 12:24:16 +0700

    add filter by category, brand for products

--- src/main/java/com/example/shopdemo/controller/HomeController.java
+++ src/main/java/com/example/shopdemo/controller/HomeController.java
@@ -1,20 +1,30 @@
 package com.example.shopdemo.controller;
 
 import com.example.shopdemo.pojo.ProductSpecs;
+import com.example.shopdemo.service.BrandService;
+import com.example.shopdemo.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private BrandService brandService;
+    private CategoryService categoryService;
+
+    @Autowired
+    public HomeController(BrandService brandService, CategoryService categoryService) {
+        this.brandService = brandService;
+        this.categoryService = categoryService;
+    }
+
     @GetMapping("")
     public String showHomepage(ProductSpecs specs, Model model) {
         model.addAttribute("specs", specs);
+        model.addAttribute("brands", brandService.getAllBrands(PageRequest.of(0, 100)));
+        model.addAttribute("categories", categoryService.getAllCategories(PageRequest.of(0, 100)));
         return "index";
-    }
-
-    @GetMapping("/nav")
-    public String showNavigationMenu() {
-        return "fragments/navigation";
     }
 }
